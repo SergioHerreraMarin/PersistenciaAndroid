4319fd4a4508d62b2164eb4781b1e43dec8bb540
SergioHerreraMarin <sergioherreramarin@gmail.com>
2022-11-07 16:28:42 +0100

    Toast added

--- app/src/main/java/com/example/persistenciaandroid/MainActivity.java
+++ app/src/main/java/com/example/persistenciaandroid/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.File;
 import java.io.FileWriter;
@@ -30,26 +31,27 @@ public class MainActivity extends AppCompatActivity {
 
         textView.setText("Text: " + getData());
 
+        String text = "File Saved";
+        Toast toast = Toast.makeText(this, text, Toast.LENGTH_LONG);
+
         buttonSave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String data = inputText.getText().toString();
-                saveData(data);
+                saveData(data, toast);
             }
         });
-
     }
-
-
-    private void saveData(String data){
+    
+    private void saveData(String data, Toast toast){
         try{
-
             File path  = this.getFilesDir();
             File file = new File(path, "dades.txt");
             FileWriter writer = new FileWriter(file);
             writer.write(data);
             writer.flush();
             writer.close();
+            toast.show();
 
         }catch (Exception e){
             e.printStackTrace();
